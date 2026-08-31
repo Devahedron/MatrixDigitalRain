3a54ff1730aa3e14449e61cab03e6ca32748b764
Declan Garvey <160069723+Devahedron@users.noreply.github.com>
2026-08-30 21:55:20 -0400

    Add draw interval

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
 let columns = 80;
-let rows = columns/2;
+let rows = columns;
 
 xIncrement = canvas.width/columns;
 yIncrement = canvas.height/rows;
@@ -35,6 +35,7 @@ function drawCharAtLoc(string, x, y) {
 }
 
 function drawAll() {
+    clearAll();
     for (var x = 0; x < columns; x++) {
         for (var y = 0; y < rows; y++) {
             drawCharAtLoc(randomString(1), x, y);
@@ -46,4 +47,16 @@ function clearAll() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-drawAll();
+let rainDrops = [];
+let rainDropLength = 10;
+
+function rainDrop() {
+    drawCharAtLoc(randomString(1), (Math.floor(Math.random() * columns)), 0)
+}
+
+function draw() {
+    clearAll();
+    rainDrop();
+}
+
+drawInterval = setInterval(drawAll, 100);
