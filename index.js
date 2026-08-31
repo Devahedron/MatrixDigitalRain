c2e6c4ff5886edaa582607ba746e1d7771243560
Declan Garvey <160069723+Devahedron@users.noreply.github.com>
2026-08-30 20:08:52 -0400

    Update drawCharAtLoc func to support grid input

--- index.js
+++ index.js
@@ -17,12 +17,21 @@ const ctx = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-let columns = 100;
+let columns = 10;
+let rows = 50;
+
+xIncrement = canvas.width/columns;
+yIncrement = canvas.height/rows;
 
 function drawCharAtLoc(string, x, y) {
     ctx.fillStyle = "#03A062";
     ctx.font = 150/columns + "vw Monocraft";
-    ctx.fillText(string, 0+x, (canvas.height*3/columns)+y);
+    ctx.fillText(string, (x+1/6)*(canvas.width/columns), (canvas.width*(8/6)/columns)+(y*(canvas.width*(8/6)/columns)));
 }
 
-drawCharAtLoc("Hello World!", 0, 0);
+drawCharAtLoc(randomString(1), 0, 0);
+drawCharAtLoc(randomString(1), 0, 1);
+drawCharAtLoc(randomString(1), 1, 0);
+drawCharAtLoc(randomString(1), 2, 1);
+drawCharAtLoc(randomString(1), 3, 2);
+drawCharAtLoc(randomString(1), 4, 3);
